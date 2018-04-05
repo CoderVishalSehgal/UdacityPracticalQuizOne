ccb460c4971106d6d68ba6f7a8c19125f4992711
Vishal Sehgal <codervishalsehgal@gmail.com>
2018-04-05 10:36:47 +0530

    Initial commit

---
+++ app/src/main/java/com/developervishalsehgal/udacitypracticalquizone/DetailsActivity.java
@@ -0,0 +1,30 @@
+package com.developervishalsehgal.udacitypracticalquizone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar()!=null)
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == android.R.id.home){
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
